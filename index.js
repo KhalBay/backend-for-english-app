7f5b8296203ad089a4d1426f46b82e553609adc6
Egor <jagyar37@gmail.com>
2025-03-15 16:20:46 +0800

    update DB

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-// })
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// })
 
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
