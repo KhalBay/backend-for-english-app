673371d0ae5212caa39fd587e49fecb52ba4191b
Egor <jagyar37@gmail.com>
2025-03-16 12:37:19 +0800

    test connect

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-// })
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false,
     },
